76456cd49e58f840ac27f10aaf6208b8178ec4f9
Rich <yalzalceconstante@yahoo.com>
2025-05-21 16:11:21 +0800

    new

--- src/types/automata.ts
+++ src/types/automata.ts
@@ -29,6 +29,6 @@ export interface SampleData {
 }
 
 export const regexSamples = {
-  sample1: "(aa+bb) (aba+bab+bbb) (a+b)* (aa+bb) (aa+bb)* (ababa) (ababa)* (bbb+aaa) (a+b)*",
-  sample2: "(1* 0 1* 0 1) (11+00) (10+01) (1+0) (11+00) (1+0+11+00+101+111+000) (11+00)* (10* 10* 1) (11+00)*"
+  sample1: "(aa+bb)(aba+bab+bbb)(a+b)*(aa+bb)(aa+bb)*(ab*ab*a)(ab*ab*a)*(bbb+aaa)(a+b)*",
+  sample2: "(1* 01* 01*) (11+00) (10+01)* (1+0) (11+00) (1+0+11+00+101+111+000) (11+00)* (10* 10* 1) (11+00)*"
 };
